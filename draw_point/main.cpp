3616385815eadd29904e4d1f7a6b5a6a41419fed
ggjulio <juligonz@student.42.fr>
2021-04-24 16:27:27 +0200

    .

--- draw_point/main.cpp
+++ draw_point/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <unistd.h>
 
 #define SCREEN_WIDTH	640
 #define SCREEN_HEIGHT	480
@@ -25,22 +26,16 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	float vertices[] =
-	{
-		-0.5, 0.5, 0.0,      
-		0.5, 0.5, 0.0, 
-		0.5, -0.5, 0.0, 
-		-0.5, -0.5, 0.0
-	};
 
 	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	// glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
-	glOrtho(-1, 1, -1, 1, 0, 1);
+	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
+	GLfloat pointVertex[] = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+	GLfloat pointVertex2[] = {SCREEN_WIDTH * 0.75, SCREEN_HEIGHT / 2};
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -48,13 +43,23 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		/* Render here */
+		glEnable( GL_POINT_SMOOTH );
 		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(3, GL_FLOAT, 0, vertices);
-		glDrawArrays(GL_QUADS, 0, 4);
+		glPointSize( 150 ); 
+		glVertexPointer(2, GL_FLOAT, 0, pointVertex);
+		glDrawArrays(GL_POINTS, 0, 1);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisable( GL_POINT_SMOOTH );
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(2, GL_FLOAT, 0, pointVertex2);
+		glPointSize( 120 ); 
+		glDrawArrays(GL_POINTS, 0, 1);
 		glDisableClientState(GL_VERTEX_ARRAY);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
+		usleep(3000 * 1000);
 
 		/* Poll for and process events */
 		glfwPollEvents();
