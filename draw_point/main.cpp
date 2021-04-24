389ad82970669bb979fd561f1ae247c341654917
ggjulio <juligonz@student.42.fr>
2021-04-24 15:55:51 +0200

    .

--- draw_point/main.cpp
+++ draw_point/main.cpp
@@ -33,26 +33,13 @@ int main(void)
 		-0.5, -0.5, 0.0
 	};
 
-	// specify the part of the window to which opengl will draw (in pixels), convert from normalized to pixels
 	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	// projection matrix defines the properties of the camera that views the objects in the world coordinate frame,
-	// here you typically set the zoom factor, aspect ratio and the near and far clipping planes.
 	glMatrixMode(GL_PROJECTION);
-
-	// replace the current matrix with the identity matrix and starts us a fresh because
-	// matrix transforms such as glOrtho and glRotate cumulate, basically puts us at (0, 0, 0)
 	glLoadIdentity();
-	
- 	// essentially set coordinate system
+	// glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
 	glOrtho(-1, 1, -1, 1, 0, 1);
-//  glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
-	
-	// (default matrix mode) modelview matrix defines how your objects are
-	// transformed in our world (meaning translation, rotation and scaling)
 	glMatrixMode(GL_MODELVIEW);
-	
-	glLoadIdentity(); // same as the other call
+	glLoadIdentity();
 
 
 	/* Loop until the user closes the window */
