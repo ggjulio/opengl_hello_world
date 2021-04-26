5d92f78ef94cba1e5186bff0e528b7eb98622bab
ggjulio <juligonz@student.42.fr>
2021-04-26 15:47:33 +0200

    modern gl: error: 'GLSL 3.30 is not supported.'    shit here we go again bumblebee and discrete gpu. What a shitty laptop. (trying to do some shaders...)

--- modern_opengl/main.cpp
+++ modern_opengl/main.cpp
@@ -8,6 +8,46 @@
 #define SCREEN_WIDTH	640
 #define SCREEN_HEIGHT	480
 
+unsigned int compileShader(GLuint type, const std::string &source)
+{
+	GLuint id = glCreateShader(type);
+	const char * src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char *message = (char *)alloca(length);
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cerr << "failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")  << " shader !" << std::endl
+			<< message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+
+static GLuint createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	GLuint program = glCreateProgram();
+	GLuint vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+	GLuint fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+	
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -31,16 +71,47 @@ int main(void)
 
 	std::cout << "gl version: " << glGetString(GL_VERSION) << std::endl;
 
-	unsigned int a;
-	glGenBuffers(1, &a);
-
-	GLfloat triangleVertices[] = 
+	GLfloat triangleVertices[9] = 
 	{
-		0, 0.5, 0,
+		 0,		0.5, 0,
 		-0.25, -0.5, 0,
-		0.25, -0.5, 0
+		 0.25, -0.5, 0
 	};
 
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triangleVertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (const void *)0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	std::string vertexShader = 
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"  gl_Position = position;\n"
+		"}\n"
+	;
+
+	std::string fragmentShader = 
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"  color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n"
+	;
+
+	GLuint shader = createShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -48,10 +119,7 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Render here */
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(3, GL_FLOAT, 0, triangleVertices);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDisableClientState(GL_VERTEX_ARRAY);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
